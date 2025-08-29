e3332fd895e07123c91f5957a2921c3ffc491699
nic1908 <niclas.roemer@icloud.com>
2025-08-29 23:10:03 +0200

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        youraIvory: "#F5EFE7",
+        youraPlum: "#2B1834",
+        youraCocoa: "#3B221E"
+      }
+    }
+  },
+  plugins: []
+};
